6a2ca203c8fafd0401dc39e9c16c3a5ae5108ee0
Shaw <shshaw@users.noreply.github.com>
2016-05-05 17:08:15 -0500

    XMLHttpRequest for compatibility ajax.

--- svg-inject.js
+++ svg-inject.js
@@ -9,55 +9,75 @@
 })(this, function (){
 
   var slice = Array.prototype.slice;
-  
+
+  function ajax(url, callback){
+    var request,
+        error = function(err){
+          callback(null,new Error(err));
+        };
+
+    request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.onreadystatechange = function() {
+      if (4 === this.readyState) {
+        if ( this.status >= 200 && this.status <= 400 ) {
+          callback(this.responseText);
+        } else { error(this.responseText); }
+      }
+    };
+    request.onerror = error;
+    request.send();
+
+    return request;
+  }
+
   function inject(element, callback) {
     callback = callback || function(){};
-  
-    var url = element.getAttribute('src')
+
+    var url = element.getAttribute('src'),
+        attributes = {};
+
     if ( !url ) return;
-  
-    var attributes = slice.call(element.attributes).map(function(attr) {
-      return [attr.name, attr.value];
-    })
-  
+
+    slice.call(element.attributes).map(function(attr) {
+      attributes[attr.name] = attr.value;
+    });
+
     element.style.opacity = 0;
-  
-    return fetch(url, { method: 'get' })
-      .then(function(res) {
-        return res.text();
-      }).then(function(res){
+
+    return ajax(url,function(res){
         var div = document.createElement('div');
         div.innerHTML = res;
-  
+
         var svg = div.firstChild;
-        for (var i = 0; i < attributes.length; i++) {
-          svg.setAttribute(attributes[i][0], attributes[i][1])
+        for ( var key in attributes ){
+          svg.setAttribute(key,attributes[key]);
         }
-  
+
         if (element.parentNode) {
           element.parentNode.replaceChild(svg, element)
         }
-  
+
         callback(svg);
-      })
-      .catch(function(err){
-        callback(null,new Error(err));
       });
   }
-  
+
+  inject.ajax = ajax;
+
   inject.all = function(obj,callback) {
     obj = ( typeof obj === 'string' ? document.querySelectorAll(obj) :
             obj instanceof Node ? [obj] : obj );
-  
+
     var length = obj.length,
         i = 0;
-  
+
     for ( ; i < length; i++ ) {
       if ( inject.call( obj[i], obj[ i ], callback ) === false ) { break; }
     }
-  
+
     return obj;
   };
 
   return inject;
+
 });
